49a1434c2bfb36bbf4773e4621c7ea0bbff7f57b
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-13 16:07:40 +0530

    root login register routes add

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 const port = 3000;
-app.listen(3000, ()=>{
-    console.log(`Server started on port ${port}`);
+app.listen(3000, () => {
+  console.log(`Server started on port ${port}`);
 });
